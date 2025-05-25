d772b6cf1041d993d5a29f5a7509965e197bfd87
TUFFNUF <brandon.findlay@yahoo.ca>
2025-05-24 22:03:20 -0400

    Update ai_concierge.py
    changed the temperature

--- ai_concierge.py
+++ ai_concierge.py
@@ -91,7 +91,7 @@ if send and user_input:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=messages,
-            temperature=0.2  # Less creativity = fewer hallucinations
+            temperature=0.4  # Less creativity = fewer hallucinations
         )
 
         reply = response.choices[0].message.content
